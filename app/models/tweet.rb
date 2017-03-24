9a54ee0715827a8ff0e879c8dfd9eac870be5330
Gus Hobbs <gus.hobbs@gmail.com>
2017-03-24 14:37:38 -0400

    Add image upload to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
   validates :user, presence: true
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
